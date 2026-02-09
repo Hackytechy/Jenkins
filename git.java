f807e6d2c15df3b32bdfd2e046f12cff4e58e8de
Hackytechy <1032210658@tcetmumbai.in>
2026-02-09 23:58:55 +0530

    first commit

---
+++ git.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int firstNum = 10;
+        int secondNum = 20;
+        int sum = firstNum + secondNum;
+
+        System.out.println("The total is: " + sum);
+    }
+}
