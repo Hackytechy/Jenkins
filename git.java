4c8ef44dabd31a878550c501b4634ffed4b4f5d5
Hackytechy <1032210658@tcetmumbai.in>
2026-02-10 20:52:35 +0530

    Updated commit

--- git.java
+++ git.java
@@ -1,9 +1,18 @@
+// public class git {
+//     public static void main(String[] args) {
+//         int firstNum = 10;
+//         int secondNum = 20;
+//         int sum = firstNum + secondNum;
+
+//         System.out.println("The total is: " + sum);
+//     }
+// }
+
 public class git {
     public static void main(String[] args) {
-        int firstNum = 10;
-        int secondNum = 20;
+        int firstNum = 50;
+        int secondNum = 100;
         int sum = firstNum + secondNum;
-
         System.out.println("The total is: " + sum);
     }
 }
